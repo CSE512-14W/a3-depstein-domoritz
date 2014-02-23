d888ee150ca42fd2bf3ec0264caacb6a65e997d9
Dominik Moritz <domoritz@gmail.com>
2014-02-23 11:30:31 -0800

    Map style is from open cycle map

--- scripts/map.js
+++ scripts/map.js
@@ -1,7 +1,7 @@
 var map = (function() {
 	var osm = L.tileLayer('http://{s}.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors,' +
-            'tiles from <a href="http://www.opencyclemap.org/">OpenTransportMap</a>'
+            'tiles from <a href="http://www.opencyclemap.org/">OpenCycleMap</a>'
     });
 
     var map = L.map('map', {
